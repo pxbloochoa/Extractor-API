a9aea6fea6bc39c5dc89680b6cfb8a7cba0ab87a
pablo-ochoa <pxbloochoa@gmail.com>
2020-10-05 11:51:15 -0500

    Repositorio para la base de datos

---
+++ extractor/src/main/java/com/cdmx/informations/extractor/repository/MetrobusDataRepository.java
@@ -0,0 +1,8 @@
+package com.cdmx.informations.extractor.repository;
+
+import com.cdmx.informations.extractor.model.MetrobusData;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MetrobusDataRepository  extends CrudRepository<MetrobusData, Integer > {
+
+}
