6f223db89aa99aa9e3579ef61b1550962797dd8a
pablo-ochoa <pxbloochoa@gmail.com>
2020-10-05 22:04:14 -0500

    Controlador de datos

---
+++ data-locations-cdmx/src/main/java/com/cdmx/informations/datalocations/controller/ControllerData.java
@@ -0,0 +1,35 @@
+package com.cdmx.informations.datalocations.controller;
+
+import graphql.ExecutionInput;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+//@RestController
+//@RequestMapping("/api/station")
+public class ControllerData {
+
+	@Autowired
+	private GraphQL graphQL;
+
+	@SneakyThrows
+	@PostMapping
+	public ResponseEntity<Object> station (@RequestBody String query){
+		JSONObject json = new JSONObject(query);
+		log.info(json.getString("query"));
+		ExecutionResult execute = graphQL.execute(ExecutionInput.newExecutionInput().query(json.getString("query")).build());
+		return new ResponseEntity<>(execute,HttpStatus.OK);
+	}
+
+}
