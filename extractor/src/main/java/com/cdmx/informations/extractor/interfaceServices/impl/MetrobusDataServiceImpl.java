721a1a8cadb371e7e1e5dbf0f8d6ee2c974f3a99
pablo-ochoa <pxbloochoa@gmail.com>
2020-10-05 21:49:08 -0500

    Corrección de errores

--- extractor/src/main/java/com/cdmx/informations/extractor/interfaceServices/impl/MetrobusDataServiceImpl.java
+++ extractor/src/main/java/com/cdmx/informations/extractor/interfaceServices/impl/MetrobusDataServiceImpl.java
@@ -1,33 +1,35 @@
 package com.cdmx.informations.extractor.interfaceServices.impl;
 
-import com.cdmx.informations.extractor.interfaceServices.IMetrobusDataServices;
+import com.cdmx.informations.extractor.interfaceServices.DataServicesInterface;
 import com.cdmx.informations.extractor.repository.MetrobusDataRepository;
 import com.cdmx.informations.extractor.model.MetrobusData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class MetrobusDataServiceImpl implements IMetrobusDataServices {
+@Qualifier("MetrobusDataServiceImpl")
+public class MetrobusDataServiceImpl implements DataServicesInterface {
 
     @Autowired
     MetrobusDataRepository repository;
 
     @Override
-    public List<MetrobusData> listDataMetrobus() {
+    public List<Object> listData() {
         return null;
     }
 
     @Override
-    public Optional<MetrobusData> listMetrobusDataID(int id) {
+    public Optional<Object> listDataID(int id) {
         return Optional.empty();
     }
 
     @Override
-    public int save(MetrobusData data) {
-        repository.save(data);
+    public int save(Object data) {
+        repository.save((MetrobusData)data);
         return 0;
     }
 
