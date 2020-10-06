81f15e53b6ca4e87520652677d0eeaa8f87cd5c5
pablo-ochoa <pxbloochoa@gmail.com>
2020-10-05 21:50:03 -0500

    Se agrego un cron para las tareas de extración periodica

--- extractor/src/main/java/com/cdmx/informations/extractor/ExtractorApplication.java
+++ extractor/src/main/java/com/cdmx/informations/extractor/ExtractorApplication.java
@@ -1,25 +1,37 @@
 package com.cdmx.informations.extractor;
 
-import com.cdmx.informations.extractor.controller.ExtractorController;
-import com.cdmx.informations.extractor.interfaceServices.IMetrobusDataServices;
+import com.cdmx.informations.extractor.controller.MetrobusDataController;
+import com.cdmx.informations.extractor.controller.TownHallDataController;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
+@ConfigurationProperties("app.config")
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {"com.cdmx.informations.extractor"})
 public class ExtractorApplication implements  CommandLineRunner{
+
+	//@Value("${app.config.scheduler}")
 
 
 	@Autowired
 	private RestTemplate restTemplate;
 
 	@Autowired
-	ExtractorController extractorController;
+	MetrobusDataController extractorController;
 
+	@Autowired
+	TownHallDataController townHallDataController;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExtractorApplication.class, args);
@@ -36,9 +48,18 @@ public class ExtractorApplication implements  CommandLineRunner{
 
 	@Override
 	public void run(String ... args){
-		System.out.println("inicia--------------------");
+
+	}
+
+	@Scheduled(fixedRateString = "${time.scheduled}")
+	public void Tasks(){
+		log.info("Starting Task Extract Data Location from metrobus");
 		extractorController.getDataLocation(restTemplate);
-		System.out.println("Termina--------------------");
+		log.info("Terminated Task Extract Data Location from metrobus");
+		log.info("Starting Task Extract To Data Town Hall");
+		townHallDataController.getDataLocation(restTemplate);
+		log.info("Terminated Task Extract To Data Town Hall");
+
 	}
 
 
