325d938a904dc46af66faa60fbadc2cba43e2874
pablo-ochoa <pxbloochoa@gmail.com>
2020-10-05 21:47:41 -0500

    Repositorio de datos para la DB de alcaldias

---
+++ extractor/src/main/java/com/cdmx/informations/extractor/repository/TownHallDataRepository.java
@@ -0,0 +1,7 @@
+package com.cdmx.informations.extractor.repository;
+
+import com.cdmx.informations.extractor.model.TownHallDataModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TownHallDataRepository extends CrudRepository<TownHallDataModel, Integer> {
+}
