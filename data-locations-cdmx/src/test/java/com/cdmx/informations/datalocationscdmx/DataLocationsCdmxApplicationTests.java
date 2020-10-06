60498f3624b2b1af989571b49a4dbdea5e372467
pablo-ochoa <pxbloochoa@gmail.com>
2020-10-05 22:05:25 -0500

    Testing Class

---
+++ data-locations-cdmx/src/test/java/com/cdmx/informations/datalocationscdmx/DataLocationsCdmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdmx.informations.datalocationscdmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLocationsCdmxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
